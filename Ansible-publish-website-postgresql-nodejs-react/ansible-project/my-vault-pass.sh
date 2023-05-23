257d7152d375c1eb096d363ec7e846649e6ba09e
latifyildirim <lyildirim.3017@gmail.com>
2023-05-23 19:38:12 +0200

    fertig :)

--- Ansible-publish-website-postgresql-nodejs-react/ansible-project/my-vault-pass.sh
+++ Ansible-publish-website-postgresql-nodejs-react/ansible-project/my-vault-pass.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
 aws --region=us-east-1 ssm get-parameters --names "my-passwd" --query "Parameters[*].{Value:Value}" --output text
+
+# # ansible-playbook play-newrole.yml --vault-password-file ./my-vault-pass.sh
